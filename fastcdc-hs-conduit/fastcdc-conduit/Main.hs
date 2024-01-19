08d07ae24a68a70719ea5598a46e9b19ad7761dc
Sander <hey@sandydoo.me>
2024-01-19 07:32:48 +0000

    Add basic conduit app

---
+++ fastcdc-hs-conduit/fastcdc-conduit/Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import Conduit
+import Data.Conduit.FastCDC.V2020
+import FastCDC.V2020 as FastCDC
+import System.CPUTime
+import System.Directory (getFileSize)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  path <-
+    getArgs >>= \args -> case args of
+      [path] -> return path
+      _ -> error "Usage: fastcdc-conduit <path>"
+
+  -- Compute the size of the file
+  fsSize <- getFileSize path
+
+  start <- getCPUTime
+
+  size <-
+    runConduitRes $
+      sourceFile path
+        .| fastCDC (FastCDCOptions 4096 8192 16384)
+        -- Fetch the len of each Chunk and sum them up
+        .| foldlC (\acc c -> acc + len c) 0
+
+  end <- getCPUTime
+  let duration :: Double = fromIntegral (end - start) / (10 ^ (12 :: Integer))
+
+  putStrLn $
+    unlines
+      [ "File size: " <> show fsSize <> " bytes",
+        "Sum of chunks: " <> show size <> " bytes",
+        "Duration: " <> show duration <> " seconds"
+      ]
